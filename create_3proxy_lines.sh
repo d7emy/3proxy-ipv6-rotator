f709c9d357aec0af7cd49adfe9529fbd0e6f13a4
d7emy <74424217+d7emy@users.noreply.github.com>
2022-12-24 20:34:37 +0300

    Update create_3proxy_lines.sh

--- create_3proxy_lines.sh
+++ create_3proxy_lines.sh
@@ -1,9 +1,10 @@
 port=30000
 count=1
+ip="127.0.0.1"
 while IFS= read -r line3 <&3 || ((eof3=1))
       !((eof3))
 do
-                echo "proxy -6 -n -a -ocUSE_TCP_FASTOPEN -p$port -i45.159.189.78 -e$line3"
+                echo "proxy -6 -n -a -ocUSE_TCP_FASTOPEN -p$port -i$ip -e$line3"
         ((port+=1))
         ((count+=1))
         if [ $count -eq 90001 ]; then
